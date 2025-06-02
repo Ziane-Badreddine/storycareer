c803b7f161ea3ffcb5cbcf3dfd9ed33fa369f7ee
Ziane-Badreddine <zianebadredddine2004@gmail.com>
2025-06-02 14:20:03 +0100

    fix

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -4,10 +4,11 @@ import { NextResponse } from 'next/server';
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
-    const user = await clerkClient.users.getUser(params.id);
+    const {id} = await params
+    const user = await clerkClient.users.getUser(id);
     return NextResponse.json(user);
   } catch (error) {
     console.error('Erreur lors de la récupération de l\'utilisateur:', error);
